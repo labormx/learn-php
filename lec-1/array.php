0c166abadc1dd554d3bee0785712bfbd2db4b266
labormx <labormx@greenhouse9.com>
2018-04-29 16:32:38 +0900

    array

---
+++ lec-1/array.php
@@ -0,0 +1,18 @@
+<?php
+// 配列
+$data = array(
+    0 => "PHP",
+    1 => "Java",
+    2 => "Python",
+    3 => "Pascal"
+);
+print($data[3]."\n");
+
+$data2 = array("神奈川" => "横浜", "埼玉" => "さいたま", "群馬" => "高崎");
+print($data2["埼玉"]);
+$data3 = array("東京", "神奈川", "埼玉");
+$data3[0] = "千葉";
+$data3[] = "群馬";
+print_r($data3);
+?>
+
