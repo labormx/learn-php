0e663ac8db9aeab393a1893f1fae2baa71bdc98b
labormx <labormx@greenhouse9.com>
2018-04-29 16:16:34 +0900

    check variables

--- lec-1/variable.php
+++ lec-1/variable.php
@@ -10,4 +10,20 @@
   print __FILE__."\n";
   print __DIR__."\n";
 
+  $name = "一太郎";
+
+  $data1 = "こんばんは、${name}さん\n";
+  print($data1);
+  $data2 = 'こんばんは、${name}さん';
+  print($data2);
+
+  $x = 10 / 3;
+  $y = 10 % 3;
+  $z = 10.1 /3;
+  $w = 10.1 % 3;
+  var_dump($x);
+  var_dump($y);
+  var_dump($z);
+  var_dump($w);
+
   ?>
